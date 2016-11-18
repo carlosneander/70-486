acfc336167dd4d3e5d459a06456c2060f491c9ac
Carlos Henrique G. de Araújo <cadastrosapenas@gmail.com>
2016-11-18 14:09:00 -0200

    Update AccountViewModels.cs

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -1,8 +1,33 @@
 ﻿using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 using System.ComponentModel.DataAnnotations;
 
 namespace CadeMeuMedico.Models
 {
+
+static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+
     public class ExternalLoginConfirmationViewModel
     {
         [Required]
@@ -56,7 +81,18 @@ namespace CadeMeuMedico.Models
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Hash da senha")]
-        public string PasswordHash { get; set; }
+        public string PasswordHash
+        {
+            get
+            {
+                return PasswordHash;
+             }
+             set
+             {
+                
+                PasswordHash = GetMd5Hash(md5Hash, value);
+             }
+        }
 
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
